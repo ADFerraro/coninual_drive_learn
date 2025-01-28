5057bad75a7c48285760d7b555dd18bfa07d0954
ADFerraro <antoniodaniele.ferraro@studenti.unisalento.it>
2025-01-28 11:00:10 +0100

    Update python pull script
    Now the docker image is automatically pulled
    Added --run flag to run the image inside a container

--- scripts/pull.py
+++ scripts/pull.py
@@ -1,5 +1,16 @@
 import os
 import subprocess
+import argparse
+
+repository_path = '~/Desktop/Tesi/code/continual_drive_learn/'
+
+parser = argparse.ArgumentParser(description="Pull git repository and docker image")
+parser.add_argument(
+    "-r",
+    "--run",
+    action="store_true",
+    help="run the image inside a container",
+)
 
 def git_pull(repo_path):
     if os.path.isdir(os.path.expanduser(repo_path)):
@@ -11,5 +22,22 @@ def git_pull(repo_path):
     else:
         print(f'Error: {repo_path} is not a valid directory')
 
-repository_path = '~/Desktop/Tesi/code/continual_drive_learn/'
+def docker_pull():
+    try:
+        subprocess.run(['docker', 'pull', 'adferraro/continual_drive'], check=True)
+        print(f'Succesfully pulled image')
+     except subprocess.CalledProcessError as e:
+        print(f'Error pulling docker image: {e}')
+
+
 git_pull(repository_path)
+docker_pull()
+if args.run:
+    try:
+        p = subprocess.Popen(['docker', 'run','--name','testcontainer', '-it', 'adferraro/continual_drive'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Error running container: {e}')
+    except KeyboardInterrupt:
+        p.kill()
+        os.exit()
+
