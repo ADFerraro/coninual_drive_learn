d8f723300d936650fa76a904bf5dc72d4112f1ed
ADFerraro <antoniodaniele.ferraro@studenti.unisalento.it>
2025-02-05 17:49:02 +0100

    added graphical help while collecting data

--- scripts/data.py
+++ scripts/data.py
@@ -3,12 +3,13 @@ from datetime import datetime
 import sys
 import time
 import subprocess
-#import keyboard
+import keyboard
 import json
-#from PIL import Image
+from PIL import Image
 import shutil
 import warnings
 import argparse
+from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(description="Collect a set of samples from the board")
 parser.add_argument(
@@ -51,10 +52,13 @@ IMAGE_NAME = "imx390_isp_1920_1080.ppm"
 BOARD_IP = "192.168.0.20"
 counter=0
 queue=[]
+sig_queue=[[],[],[]]
+plt.ion()
+fig, axs = plt.subplot_mosaic([['img', 'accel'],['img', 'brake'],['img', 'steer']],layout = 'constrained')
 
 date=datetime.now().strftime("%Y-%m-%d-%H.%M.%S.%f")
-#keyboard.on_press_key("w", lambda _: on_w())
-#keyboard.on_press_key("s", lambda _: on_s())
+keyboard.on_press_key("w", lambda _: on_w())
+keyboard.on_press_key("s", lambda _: on_s())
 
 with open(os.path.abspath("..\..\log.json"), "w") as f:
     json.dump([],f)
@@ -77,10 +81,45 @@ while True:
     try:
         date=datetime.now().strftime("%Y-%m-%d-%H.%M.%S.%f")
         p=subprocess.run(f"scp root@{BOARD_IP}:/home/root/frames/{IMAGE_NAME} "+os.path.abspath(f"..\..\images\img_{date}.ppm"),shell=True)
-        Image.open(os.path.abspath(f"..\..\images\img_{date}.ppm")).save(os.path.abspath(f"..\..\images\img_{date}.png"),"PNG")
+        image = Image.open(os.path.abspath(f"..\..\images\img_{date}.ppm"))
+        image.save(os.path.abspath(f"..\..\images\img_{date}.png"),"PNG")
         
+        # Plotting what necessary on a new frame
+        sig_queue[0].append(acc)
+        sig_queue[1].append(brake)
+        sig_queue[2].append(steer)
+
+
+        if(len(sig_queue[0]) > 10):
+            for q in sig_queue:
+                q.pop(0)
+        
+        axs['img'].clear()
+        axs['img'].set_title('image')
+        axs['img'].imshow(image)
+
+        
+        axs['accel'].clear()
+        axs['accel'].set_title('acceleration')
+        axs['accel'].plot(sig_queue[0])
+        
+        axs['brake'].clear()
+        axs['brake'].set_title('brake')
+        axs['brake'].plot(sig_queue[1])
+
+        axs['steer'].clear()
+        axs['steer'].set_title('steer')
+        axs['steer'].plot(sig_queue[2])
+        
+        plt.pause(0.01)            
+
+        # Removing ppm file 
         os.remove(os.path.abspath(f"..\..\images\img_{date}.ppm"))
-        queue.append(f"img_{date}.png")
+
+        # Appending to queue if necessary
+        if(args.samples_number):
+            queue.append(f"img_{date}.png")
+
         # Shift images if the maximum number is reached
         if(args.samples_number and (counter >= args.samples_number)):
             os.remove(os.path.abspath(f"..\..\images\\"+queue[0]))
@@ -92,6 +131,7 @@ while True:
 
         data.append({"img": f"img_{date}.png", "throttle": acc, "brake": brake, "steering": steer})
 
+        # adjusting json file if necessary
         if(args.samples_number and (counter >= args.samples_number)):
             data.pop(0)
 
